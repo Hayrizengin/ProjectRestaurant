3fcb83067a750e33674dd7dc7af47683a7a12459
Emre Kaya <emrebjk1919@gmail.com>
2025-01-07 22:14:05 +0300

    #UserManager içerisindeki metotlar dolduruldu.

--- ProjectRestaurant.Business/Concrete/UserManager.cs
+++ ProjectRestaurant.Business/Concrete/UserManager.cs
@@ -21,39 +21,152 @@ namespace ProjectRestaurant.Business.Concrete
 {
     public class UserManager : IUserService
     {
-        public Task<ApiResponse<UserDTOResponse>> AddAsync(UserDTORequest entity)
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+        private readonly IGenericValidator _validator;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly ITokenService _tokenService;
+
+        public UserManager(IGenericValidator validator, IMapper mapper, IUnitOfWork uow, IPasswordHasher passwordHasher, ITokenService tokenService)
         {
-            throw new NotImplementedException();
+            _validator = validator;
+            _mapper = mapper;
+            _uow = uow;
+            _passwordHasher = passwordHasher;
+            _tokenService = tokenService;
         }
 
-        public Task<ApiResponse<bool>> DeleteAsync(int id)
+        public async Task<ApiResponse<UserDTOResponse>> AddAsync(UserDTORequest entity)
         {
-            throw new NotImplementedException();
+            await _validator.ValidateAsync(entity,typeof(UserRegisterValidator));
+
+            var user = _mapper.Map<User>(entity);
+
+            await _uow.UserRepository.AddAsync(user);
+            await _uow.SaveChangeAsync();
+
+            var userResponse = _mapper.Map<UserDTOResponse>(user);
+            return ApiResponse<UserDTOResponse>.SuccessResult(userResponse);
         }
 
-        public Task<ApiResponse<IEnumerable<UserDTOResponse>>> GetAllAsync(UserDTORequest entity)
+        public async Task<ApiResponse<bool>> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var user = await _uow.UserRepository.GetAsync(x=>x.Id == id);
+
+            if (user == null)
+            {
+                var error = new ErrorResult(new List<string>
+                {
+                    "Silinecek kullanıcı bulunamadı."
+                });
+                return ApiResponse<bool>.FailureResult(error,HttpStatusCode.NotFound);
+            }
+
+            user.IsActive = false;
+            user.IsDeleted = true;
+            _uow.UserRepository.Update(user);
+            await _uow.SaveChangeAsync();
+
+            return ApiResponse<bool>.SuccessResult(true);
         }
 
-        public Task<ApiResponse<UserDTOResponse>> GetAsync(int id)
+        public async Task<ApiResponse<IEnumerable<UserDTOResponse>>> GetAllAsync(UserDTORequest entity)
         {
-            throw new NotImplementedException();
+            var users = await _uow.UserRepository.GetAllAsync(x=>x.IsActive == true && x.IsDeleted == false);
+
+            if (!users.Any())
+            {
+                var error = new ErrorResult(new List<string> {"Kullanıcı kaydı bulunamadı"});
+                return ApiResponse<IEnumerable<UserDTOResponse>>.FailureResult(error,HttpStatusCode.NotFound);
+            }
+
+            var userDTOResponses = _mapper.Map<IEnumerable<UserDTOResponse>>(users);
+            return ApiResponse<IEnumerable<UserDTOResponse>>.SuccessResult(userDTOResponses);
         }
 
-        public Task<ApiResponse<UserDTOResponse>> GetUserByEMailAsync(string eMailAddress)
+        public async Task<ApiResponse<UserDTOResponse>> GetAsync(int id)
         {
-            throw new NotImplementedException();
+            var user = await _uow.UserRepository.GetAsync(x=>x.Id == id);
+
+            if (user is null)
+            {
+                var error = new ErrorResult(new List<string>
+                {
+                    "Kullanıcı bulunamadı."
+                });
+                return ApiResponse<UserDTOResponse>.FailureResult(error,HttpStatusCode.NotFound);
+            }
+
+            var userDTOResponse = _mapper.Map<UserDTOResponse>(user);
+            return ApiResponse<UserDTOResponse>.SuccessResult(userDTOResponse);
+
         }
 
-        public Task<ApiResponse<LoginDTOResponse>> LoginAsync(LoginDTORequest loginRequestDTO)
+        public async Task<ApiResponse<UserDTOResponse>> GetUserByEMailAsync(string eMailAddress)
         {
-            throw new NotImplementedException();
+            var user = await _uow.UserRepository.GetAsync(x=>x.Email.ToLower() == eMailAddress.ToLower());
+
+            if (user is null)
+            {
+                var error = new ErrorResult(new List<string>
+                {
+                    "Eşleşen e-posta bulunamadı."
+                });
+                return ApiResponse<UserDTOResponse>.FailureResult(error,HttpStatusCode.NotFound);
+            }
+
+            return ApiResponse<UserDTOResponse>.SuccessResult(_mapper.Map<UserDTOResponse>(user));
         }
 
-        public Task<ApiResponse<bool>> UpdateAsync(UserDTORequest entity)
+        public async Task<ApiResponse<LoginDTOResponse>> LoginAsync(LoginDTORequest loginRequestDTO)
         {
-            throw new NotImplementedException();
+            await _validator.ValidateAsync(loginRequestDTO,typeof(LoginValidator));
+
+            var user = await _uow.UserRepository.GetAsync(x=>x.Email == loginRequestDTO.Email);
+
+            if (user == null || !_passwordHasher.VerifyPassword(user.Password,loginRequestDTO.Password))
+            {
+                var error = new ErrorResult(new List<string>{
+                    "Kullanıcı adı veya şifre yanlış."
+                });
+                return ApiResponse<LoginDTOResponse>.FailureResult(error,HttpStatusCode.NotFound);
+            }
+            else
+            {
+                var claims = new List<Claim>()
+                {
+                    new Claim("UserGuid",user.Guid.ToString()),
+                    new Claim("UserEMail",user.Email)
+                };
+                var token = _tokenService.GenerateToken(claims);
+                var loginResponseDTO = _mapper.Map<LoginDTOResponse>(loginRequestDTO);
+                loginResponseDTO.Token = token;
+                return ApiResponse<LoginDTOResponse>.SuccessResult(loginResponseDTO);
+            }
+        }
+
+        public async Task<ApiResponse<bool>> UpdateAsync(UserDTORequest entity)
+        {
+            await _validator.ValidateAsync(entity,typeof(UserUpdateValidator));
+
+            var user = await _uow.UserRepository.GetAsync(x=>x.Id == entity.Id || x.Guid == entity.Guid);
+
+            if (user is null)
+            {
+                var error = new ErrorResult(new List<string>
+                {
+                    "Güncellenecek kullanıcı bulunamadı."
+                });
+                return ApiResponse<bool>.FailureResult(error,HttpStatusCode.NotFound);
+            }
+
+            _mapper.Map(entity,user);
+
+            _uow.UserRepository.Update(user);
+            await _uow.SaveChangeAsync();
+
+            return ApiResponse<bool>.SuccessResult(true);
+
         }
     }
 }
